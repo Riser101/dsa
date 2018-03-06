91c9044e9ba671bc7826cc62bee8ca3a8d7bfad1
yousuf syed <sy.yousuf9106@gmail.com>
2018-03-06 14:55:10 +0530

    adding isort

---
+++ isort.cpp
@@ -0,0 +1,20 @@
+/**
+ *  isort implementation
+ *  dsa
+ *
+ *  input array (unsorted): 1    8    4    6    0    3    5    2    7    9
+ *  output array (sorted) : 0    1    2    3    4    5    6    7    8    9
+ *
+ *  time complexity : O(n^2)
+ *
+ *  Created by Yousuf Syed on 02/03/18.
+ *  Copyright © 2018 Yousuf Syed. All rights reserved.
+ */
+
+#include<iostream>
+#define MAX 10
+using namespace std;
+
+class isort{
+    
+};
