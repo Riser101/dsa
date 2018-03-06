e44176682b8a25684c3f26a1c49b92d60dae0a8b
yousuf syed <sy.yousuf9106@gmail.com>
2018-03-06 16:06:39 +0530

    completed isot

--- isort.cpp
+++ isort.cpp
@@ -16,5 +16,35 @@
 using namespace std;
 
 class isort{
+private:
+    int value_to_insert;
+    int hole_position;
+    int unsorted_array[MAX] = {1,8,4,6,0,3,5,2,7,9};
+public:
+    void insertion_sort(){
+        for(int i=1; i<MAX; i++){
+            hole_position = i;
+            value_to_insert = unsorted_array[i];
+            while(hole_position > 0 && unsorted_array[hole_position-1] > value_to_insert){
+                unsorted_array[hole_position] = unsorted_array[hole_position-1];
+                hole_position--;
+            }
+            if(hole_position != i){
+                unsorted_array[hole_position] = value_to_insert;
+            }
+        }
+    }
     
+    void display(){
+        for(int i=0; i<MAX; i++){
+            cout<<unsorted_array[i]<<"\t";
+        }
+        cout<<"\n";
+    }
 };
+
+int main(){
+    isort obj;
+    obj.insertion_sort();
+    obj.display();
+}
