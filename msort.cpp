45405867176171a2b93e24bfb173bff2bf4f8377
yousuf syed <sy.yousuf9106@gmail.com>
2018-03-09 23:18:03 +0530

    updated mesort

--- msort.cpp
+++ msort.cpp
@@ -1,47 +1,19 @@
-/**
- *  merge sort implementation
- *  dsa
- *
- *  input array (unsorted): 1    8    4    6    0    3    5    2    7    9
- *  output array (sorted) : 0    1    2    3    4    5    6    7    8    9
- *
- *  time complexity : O(nlogn)
- *
- *  Created by Yousuf Syed on 02/03/18.
- *  Copyright © 2018 Yousuf Syed. All rights reserved.
- */
-
 #include<iostream>
-#define MAX 10
 using namespace std;
-
 class msort{
-private:
-    int unsorted_array[MAX] = {1,8,4,6,0,3,5,2,7,9};
 public:
-    
-    void merge_sort(int array[MAX]){
-        if(sizeof(array) < 2){
+    int arr[3] = {3,2,1};
+    void sort(int low, int high){
+        if(low == high){
             return;
         }
-        
-    }
-    
-    void merge(){
-        
-    }
-    
-    void display(){
-        for(int i=0; i<MAX; i++){
-            cout<<unsorted_array[i]<<"\t";
-        }
-        cout<<"\n";
+        int mid = (low+high)/2;
+        cout<<"mid= "<<mid;
+        sort(low, mid);
     }
 };
 
 int main(){
     msort obj;
-    obj.display();
-    return 0;
+    obj.sort(0, 2);
 }
-
